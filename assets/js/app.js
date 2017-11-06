73e2e253dbf35de282431b60753aa2c4b1e0cacf
Marifuse <maria.fuenzalida.serrano@gmail.com>
2017-11-06 17:10:41 -0300

    Primera parte con evento Click, más algunas mejoras en el CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,22 @@
+//Evento Click Button y generación de un nuevo div con el Tweet
 window.onload = function() {
-  var boton = document.getElementById('click')[0];
-  var texto = document.getElementById('Escritura')[0];
+  var boton = document.getElementsByName('Tweet')[0];
+  var texto = document.getElementById('Escritura');
+  var historial = document.getElementsByName('linea')
     boton.onclick = function() {
       if (texto.value == "") {
         alert("El campo está vacío");
-
-      }
-   }
+      } else {
+        var cuadroTweet = document.createElement("div");
+        cuadroTweet.classList.add("respuesta");
+        var fecha = document.createElement("span");
+        var tweet = document.createElement("p");
+        cuadroTweet.appendChild(fecha);
+        cuadroTweet.appendChild(tweet);
+        cuadroTweet.className = "cuadro3"
+        fecha.textContent = new Date();
+        tweet.textContent = texto.value;
+        document.body.appendChild(cuadroTweet);
+        }
+    }
 }
