2ae3535faef95d6be50bb3d2671c16c7d2bd9217
Marifuse <maria.fuenzalida.serrano@gmail.com>
2017-11-07 15:17:40 -0300

    modificación HTML, CSS, y agrego de efecto conteo y colores en los números del conteo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,34 @@
+//Función conteo y cambio de colores en positivo (falta negativo).
+function conteo () {
+  var max = "140";
+	var cadena = document.getElementById("Escritura").value;
+	var longitud = cadena.length;
+
+		 if(longitud <= max) {
+		          document.getElementById("contador").value = max-longitud;
+		 } else {
+		          document.getElementById("Escritura").value = cadena.substr(0, max);
+	   }
+     if (longitud >= 120 && longitud < 130) {
+       document.getElementById('contador').style.color = "green";
+     }
+     else if (longitud >= 130 && longitud < 141) {
+       document.getElementById('contador').style.color = "orange";
+     }
+     else if (longitud > 140) {
+       document.getElementById('contador').style.color = "red";
+     } else {
+       document.getElementById('contador').style.color = "black";
+     }
+ }
 //Evento Click Button y generación de un nuevo div con el Tweet
 window.onload = function() {
   var boton = document.getElementsByName('Tweet')[0];
   var texto = document.getElementById('Escritura');
   var historial = document.getElementsByName('linea')
     boton.onclick = function() {
-      if (texto.value == "") {
-        alert("El campo está vacío");
+      if (texto.value === "" || texto.value > 140) {
+        document.getElementById('click1').disabled = false;
       } else {
         var cuadroTweet = document.createElement("div");
         cuadroTweet.classList.add("respuesta");
